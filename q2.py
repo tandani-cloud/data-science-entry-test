cf0e65261e0f316171d5dc8c976f017f5b3aaef2
tandani-cloud <tandani@gmail.com>
2026-03-25 16:24:05 +0800

    Update q2.py

--- q2.py
+++ q2.py
@@ -7,8 +7,25 @@ def find_and_replace(lst, find_val, replace_val):
     """
     return
 
+if not isinstance(lst, list):
+return "Error: Input must be a list."
+        
+for i in range(len(lst)):
+if lst[i] == find_val:
+lst[i] = replace_val
+return lst
+
+
+
+
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+
+print("--- Q2 Task 2 ---")
+print("Scenario 1:", find_and_replace([1, 2, 3, 4, 2, 2], 2, 5))
+print("Scenario 2:", find_and_replace(["apple", "banana", "apple"], "apple", "orange"))
+
+
