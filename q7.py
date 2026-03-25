b47d137a77ff0f62361aa9887c0b287078c963a8
tandani-cloud <tandani@gmail.com>
2026-03-25 16:32:15 +0800

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,4 +1,4 @@
-class Car:
+# class Car:
     """
     Task 1
     - Define a class named Car with attributes: make, model, year
@@ -6,10 +6,27 @@ class Car:
     - Add a method named describe_car() that prints information about the car as "Year Make Model"
     """
 
-    def describe_car(self):
+# def describe_car(self):
         print("")
+
+def __init__(self, make, model, year):
+self.make = make
+self.model = model
+self.year = year
+
+def describe_car(self):
+print(f"{self.year} {self.make} {self.model}")
+
+
 
 
 # Task 2
 # Create an instance of the Car class with the following attributes and call describe_car method:
 # - Make: Toyota, Model: Corolla, Year: 2020
+
+
+print("--- Q7 Task 2 ---")
+my_car = Car("Toyota", "Corolla", 2020)
+my_car.describe_car()
+
+
