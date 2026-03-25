4601ab3a56f8e41068c20b1416fd06db483f22c0
tandani-cloud <tandani@gmail.com>
2026-03-25 16:16:20 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -12,7 +12,7 @@ def swap(x, y):
 # create a function with only two variables (x and y) as inputs. If either one of x or y is not a number, the function must return -1. If both are numbers, then need to swap their values using only those two variables, and print the new swapped values.
 
 if not (isinstance(x, (int, float)) and isinstance(y, (int, float))):
-        return -1
+return -1
 
 
 
